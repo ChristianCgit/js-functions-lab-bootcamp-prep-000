bc7454cc696c99a816533b22f25c893a8f66be0e
christian.cook91@outlook.com <christian.cook91@outlook.com>
2018-07-12 14:35:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // write your code below!
-function happHolidays(string);
 happyHolidays("Happy holidays!");{
-  return happyHolidays();
-}
+  var cheer = "Happy Holidays!";
+  return happyHolidays()}
+
 
   
